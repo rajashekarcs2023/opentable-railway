c64b3b85e0909e56cd739798131f1ef1d9475008
rajashekarcs2023 <vrajashekar2022mscs@gmail.com>
2025-11-19 16:53:09 -0800

    update agent

--- agent.py
+++ agent.py
@@ -45,7 +45,7 @@ CHANNELS = 2  # Stereo
 # Create agent
 agent = Agent(
     name="real_music",
-    seed="real-music-seed-2025",
+    seed="real-music-seed2020-2025",
     port=8017,
     mailbox=True
 )
